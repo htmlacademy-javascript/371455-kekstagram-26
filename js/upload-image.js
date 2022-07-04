2adccd20ee10cc84999822a4b54d3de9444f3549
RockEnot22 <enotikav@gmail.com>
2022-07-04 23:28:25 +0200

    Правда или действие (часть 1)

---
+++ js/upload-image.js
@@ -0,0 +1,27 @@
+import {Picture,
+  FILE_TYPES} from './constants.js';
+import {overlay} from './overlay.js';
+
+const uploadButton = document.querySelector('#upload-file');
+const previewImg = document.querySelector('.img-upload__preview img');
+
+uploadButton.addEventListener('change', (evt) => {
+  const file = evt.target.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      previewImg.src = reader.result;
+      previewImg.width = Picture.WIDTH;
+      previewImg.height = Picture.HEIGHT;
+
+      overlay.open();
+    });
+
+    reader.readAsDataURL(file);
+  }
+});
